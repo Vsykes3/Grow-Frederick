1946e804188a70f5ca7d532536c5118b1fd426be
Vsykes3 <vaibhav.sykhesh24@gmail.com>
2025-07-29 18:49:48 -0400

    Archivo Font

--- grow-frederick/src/Homepage.jsx
+++ grow-frederick/src/Homepage.jsx
@@ -15,7 +15,21 @@ const Homepage = () => {
                 <button className="banner-button">Get Started</button>
             </div>
         </div>
-        <div className="features-wrapper"></div>
+        <div className="features-wrapper">
+          <p className="title">Features</p>
+          <div className="cards-container">
+            <div className="features-card">
+              <p className="features-label">Personalized Calendar</p>
+              <p className="features-content">Easily schedule and plan gardening timings with a handy calendar.</p>
+            </div>
+            <div className="features-card">
+              <p className="features-label">Real Time Pest and Weather Alerts</p>
+            </div>
+            <div className="features-card">
+              <p className="features-label">Interactive Weather Map</p>
+            </div>
+          </div>
+        </div>
         <Footer></Footer>
     </div>
   )
