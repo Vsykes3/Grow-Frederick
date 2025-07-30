7c38d61fdfb8bc520e1ed9ac14d5ac17fcb4a224
Vsykes3 <vaibhav.sykhesh24@gmail.com>
2025-07-30 14:48:44 -0400

    Homepage- Call to action and pricing complete

--- grow-frederick/src/Homepage.jsx
+++ grow-frederick/src/Homepage.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import './Homepage.css'
 import banner from './assets/Homepage Banner.jpg'
+import divider from './assets/border.png'
 
 const Homepage = () => {
   return (
@@ -18,15 +19,73 @@ const Homepage = () => {
         <div className="features-wrapper">
           <p className="title">Features</p>
           <div className="cards-container">
-            <div className="features-card">
+            <a href="/calendar" className="features-card">
               <p className="features-label">Personalized Calendar</p>
               <p className="features-content">Easily schedule and plan gardening timings with a handy calendar.</p>
-            </div>
-            <div className="features-card">
+            </a>
+            <a href="/news" className="features-card">
               <p className="features-label">Real Time Pest and Weather Alerts</p>
-            </div>
-            <div className="features-card">
+              <p className="features-content">Easily schedule and plan gardening timings with a handy calendar.</p>
+            </a>
+            <a href="/map" className="features-card">
               <p className="features-label">Interactive Weather Map</p>
+              <p className="features-content">Easily schedule and plan gardening timings with a handy calendar.</p>
+            </a>
+          </div>
+        </div>
+        <div className="call-wrapper">
+          <div className="call-section">
+            <h1 className="call-header">Get Started Today!</h1>
+          </div>
+          <div className="call-section">
+            <div className='call-right'>
+              <div className="call-button">Sign Up!</div>
+              <div className="call-button">Log In!</div>
+            </div>
+          </div>
+          <img src={divider} alt="divider" className="call-divider" />
+        </div>
+        <div className="pricing-wrapper">
+          <p className="title">Pricing</p>
+          <div className="cards-container">
+            <div className="pricing-card">
+              <div className="pricing-name">Free</div>
+              <div className="pricing-container">
+                <p className="pricing-price">$0</p>
+                <p className="pricing-subs">/forever</p>
+              </div>
+              <div className="line"></div>
+              <p className='pricing-contents'>
+                ✓ Live Weather & Outlook<br></br>
+                ✓ Full Plant & Pest Index<br></br>
+                ✓ General Planting Calendar<br></br>
+              </p>
+            </div>
+            <div className="pricing-card">
+              <div className="pricing-name">Free</div>
+              <div className="pricing-container">
+                <p className="pricing-price">$0</p>
+                <p className="pricing-subs">/forever</p>
+              </div>
+              <div className="line"></div>
+              <p className='pricing-contents'>
+                ✓ Live Weather & Outlook<br></br>
+                ✓ Full Plant & Pest Index<br></br>
+                ✓ General Planting Calendar<br></br>
+              </p>
+            </div>
+            <div className="pricing-card">
+              <div className="pricing-name">Free</div>
+              <div className="pricing-container">
+                <p className="pricing-price">$0</p>
+                <p className="pricing-subs">/forever</p>
+              </div>
+              <div className="line"></div>
+              <p className='pricing-contents'>
+                ✓ Live Weather & Outlook<br></br>
+                ✓ Full Plant & Pest Index<br></br>
+                ✓ General Planting Calendar<br></br>
+              </p>
             </div>
           </div>
         </div>
