7cbb08820c1ace900c37de727c8e1cb2f345dc2d
Your Name <your@email.com>
2025-12-20 15:19:22 -0500

    Fix Render build: Fix relative button imports to use Button.tsx

--- grow-frederick/src/components/ui/UpsellModal.tsx
+++ grow-frederick/src/components/ui/UpsellModal.tsx
@@ -1,6 +1,6 @@
 ﻿import React, { useState } from 'react';
 import { cn } from '@/lib/utils';
-import { Button } from './button';
+import { Button } from './Button';
 import { ProBadge } from './ProBadge';
 import { PLAN_PRICING } from '@/lib/plan';
 
